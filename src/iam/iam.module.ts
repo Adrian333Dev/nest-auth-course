11cf77facbfe58137931e98a3ac6e6c73a11833f
Adrian333Dev <adrian333dev@gmail.com>
2022-12-24 12:10:14 +0400

    auth guard added

--- src/iam/iam.module.ts
+++ src/iam/iam.module.ts
@@ -1,13 +1,16 @@
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtModule } from '@nestjs/jwt';
+
 import { HashingService } from './hashing/hashing.service';
 import { BcryptService } from './hashing/bcrypt.service';
 import { AuthenticationController } from './authentication/authentication.controller';
 import { AuthenticationService } from './authentication/authentication.service';
 import { User } from 'src/users/entities/user.entity';
-import { JwtModule } from '@nestjs/jwt';
 import jwtConfig from './config/jwt.config';
+import { AccessTokenGuard } from './authentication/guards/access-token/access-token.guard';
 
 @Module({
   imports: [
@@ -19,6 +22,10 @@ import jwtConfig from './config/jwt.config';
     {
       provide: HashingService,
       useClass: BcryptService,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: AccessTokenGuard,
     },
     AuthenticationService,
   ],
